30f31caa8fd6f7ea342c90a53c0da20ee68c5f59
profesormanana <manana@gmail.com>
2024-07-08 10:42:01 +0200

    8-7-2024 10:40

---
+++ 48_libreria_carrito_dtos/src/main/java/service/LibrosService.java
@@ -0,0 +1,41 @@
+package service;
+
+import java.util.List;
+
+import dao.LibrosDao;
+import dao.TemasDao;
+import dtos.LibroDto;
+import dtos.TemaDto;
+import mappers.Mapeador;
+
+public class LibrosService {
+	LibrosDao librosDao=new LibrosDao();
+	TemasDao temasDao=new TemasDao();
+	
+	public List<TemaDto> getTemas(){
+		return temasDao.findAll().stream()
+				.map(t->Mapeador.temaEntityToDto(t))//Stream<TemaDto>
+				.toList();
+	}
+	
+	public List<LibroDto> librosTema(int idTema){
+		
+		if(idTema!=0) {
+			return librosDao.findByIdTema(idTema).stream()
+					.map(l->Mapeador.libroEntityToDto(l))
+					.toList();
+		}else{
+			return librosDao.findAll().stream()
+					.map(l->Mapeador.libroEntityToDto(l))
+					.toList();
+		}
+			
+	}
+	public LibroDto getLibro(int isbn) {
+		return Mapeador.libroEntityToDto(librosDao.findByIsbn(isbn));
+	}
+	
+	public TemaDto getTema(int idTema) {
+		return Mapeador.temaEntityToDto(temasDao.findById(idTema));
+	}
+}
